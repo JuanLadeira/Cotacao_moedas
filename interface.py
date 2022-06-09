dccf4dbe94bd96d6e3b2e1e4b819cc2d3f5999cf
juan <juan.fll@hotmail.com>
2022-06-09 12:58:40 -0300

    Inclusão do tkcalendar, aplicando a função DateEntry e adicionando o botao de cotacao.

--- interface.py
+++ interface.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from tkinter import Tk
 from tkinter import ttk
+from tkcalendar import DateEntry
 from tkinter.filedialog import askopenfilename
 import pandas as pd
+
+def pegar_cotacao():
+    pass
+
 
 janela = Tk()
 lista_moeda = ["R$", "USD", "EUR"]
@@ -20,12 +25,16 @@ combobox_selecionarmoeda.grid(row=1, column=2, padx=10, pady=10, sticky="nswe")
 label_selecaododia = tk.Label(text="Selecionar o dia que deseja pegar a cotação")
 label_selecaododia.grid(row=2, column=0, padx=10, pady=10, sticky="nswe", columnspan=2)
 
-combobox_selecaododia = ttk.Combobox(values=lista_moeda)
-combobox_selecaododia.grid(row=2, column=2, padx=10, pady=10, sticky="nswe")
+calendario_moeda = DateEntry(year=2022, locale='pt_br')
+calendario_moeda.grid(row=2, column=2, padx=10, pady=10, sticky="nswe")
 
-label_dia = tk.Label(text="A cotação da moeda selecionada do dia tal é de tanto")
-label_dia.grid(row=3, column=0, padx=10, pady=10, sticky="nswe", columnspan=2)
+label_textocotacao= tk.Label(text="")
+label_textocotacao.grid(row=3, column=0, padx=10, pady=10, sticky="nswe", columnspan=2)
 
+botao_cotacao = tk.Button(text='Pegar Cotação', command=pegar_cotacao)
+botao_cotacao.grid(row=3, column=2, padx=10, pady=10, sticky="nswe")
+
+#cotação de multiplas moedas
 label_cotacaomoeda2 = tk.Label(text="cotação de multiplas moedas", borderwidth=2, relief="solid")
 label_cotacaomoeda2.grid(row=4, column=0, padx=10, pady=10, sticky="nswe", columnspan=3)
 janela.mainloop()
